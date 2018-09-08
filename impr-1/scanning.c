54058832770a07f061d7e1eaa7b7334acc585f4c
Ane Søgaard Jørgensen <42242841+Kaliahh@users.noreply.github.com>
2018-09-08 21:31:08 +0200

    Upload from laptop

--- impr-1/scanning.c
+++ impr-1/scanning.c
@@ -1,19 +1,14 @@
 # include <stdio.h>
 
 int main(void) {
-  float a;
 
-  printf("Input a number: ");
+  float a = 0;
+  
+  printf("Input your number: ");
   scanf("%f", &a);
-  printf("You number is: %1.1f\n", a);
+  printf("\nThis is your number: %1.1f\n",a);
 
   return(0);
 
+
 }
-
-
-
-/* 
-Altid bare hold det første argument i scanf til placeholder,
-rediger længden af decimaler og sådan i printf
-*/
