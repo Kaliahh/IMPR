a5ce0c6aa8c2459389843ee61093973cb23e958d
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2018-10-11 20:22:40 +0200

    Edited trekant-af-tal.c and added ruder-af-tal.c (WIP)

--- impr/impr-4/ruder-af-tal.c
+++ impr/impr-4/ruder-af-tal.c
@@ -1,12 +1,42 @@
 # include <stdio.h>
 
+int spaces(int);
+int top(int);
+int bottom(int);
+
 int main(void) {
 
-  int i, j, k, l, max;
+  int max;
 
   printf("Input an integer: ");
   scanf("%d", &max);
   printf("\n");
+
+  top(max);
+  bottom(max);
+
+  printf("\n");
+
+
+  spaces(max);
+
+  return 0;
+}
+
+
+int spaces(int a) {
+
+  int i;
+
+  for (i = 1; i <= a; i++) {
+    printf(" ");
+  }
+}
+
+
+int top(int max) {
+
+  int k, l;
 
   for (k = 0; k <= max - 1; k++) {
     for (l = 0; l <= k; l++) {
@@ -15,6 +45,11 @@ int main(void) {
     printf("\\");
     printf("\n");
   }
+}
+
+int bottom(int max) {
+
+  int i, j;
 
   for (i = 0; i <= max; i++) {
     for (j = 0; j <= max - i; j++) {
@@ -26,8 +61,4 @@ int main(void) {
       printf("/");
     printf("\n");
   }
-
-  printf("\n");
-
-  return 0;
 }
