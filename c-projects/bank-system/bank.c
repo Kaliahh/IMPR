f44edaf2004344bb70ba0fede5ddf8d6176a9145
Kaliahh <anesoejoe@gmail.com>
2018-10-11 08:52:46 +0200

    Small changes in bank.c

--- c-projects/bank-system/bank.c
+++ c-projects/bank-system/bank.c
@@ -1,6 +1,6 @@
 /*
  * Bank management system
- * Made by: Ane Søgaard Jørgensen
+ * Made by: Ane Sï¿½gaard Jï¿½rgensen
  * Started on: 23/09-2018
  */
 
@@ -10,43 +10,20 @@
 # include <unistd.h>
 # include "user_info.h"
 
-int new_account(void) {
-
-  char user_name[25];
-
-  FILE * fPointer;
-  printf("\nInput a username: ");
-  scanf("%s", user_name);
-
-  fPointer = fopen("users.txt", "a");
-  fprintf(fPointer, "{%s}", user_name);
-
-  fclose(fPointer);
-  
-  printf("Your username is %s\n", user_name);
-  
-}
-
-int details(void) {
-
-}
-
-int transaction(void) {
-
-}
-
+int new_account(void);
+int details(void);
+int transaction(void);
 
 int main_menu(void) {
 
   char choice[3];
 
   printf("\nVIRTUAL BANK MANAGEMENT SYSTEM\n\n########## MAIN MENU ##########\n\n");
-  
+
   printf("1. Create new account\n");
   printf("2. View details of an existing account\n");
   printf("3. Transactions\n");
 
-  
   printf("\nInput a number to proceed, or q to quit: ");
   scanf("%s", choice);
 
@@ -63,9 +40,42 @@ int main_menu(void) {
     sleep(1);
     main_menu();
   }
-    
+
   return 0;
 }
+
+
+int new_account(void) {
+
+  char user_name[25];
+
+  FILE * fPointer;
+  printf("\nInput a username: ");
+  scanf("%s", user_name);
+
+  fPointer = fopen("users.txt", "a");
+  fprintf(fPointer, "{%s}\n", user_name);
+
+  fclose(fPointer);
+
+  printf("Your username is %s\n", user_name);
+
+  main_menu();
+
+}
+
+
+int details(void) {
+
+}
+
+
+int transaction(void) {
+
+}
+
+
+
 
 
 
@@ -74,10 +84,10 @@ int main_menu(void) {
 int main(void) {
 
 
-  
+
   main_menu();
 
-  
-  
+
+
   return 0;
 }
--- c-projects/bank-system/user_info.h
+++ c-projects/bank-system/user_info.h
@@ -1,9 +1,8 @@
 struct user{
-  
+
   int password[50];
   char first_name[25];
   char last_name[25];
   int user_id[10];
   int worth;
-  
 };
