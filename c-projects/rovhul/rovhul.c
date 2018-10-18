7dc377fe7259a9ac9c95f7cbcde23893e69f9ad0
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2018-10-18 23:06:46 +0200

    Tilføjet rovhul mappe og .c og .h

---
+++ c-projects/rovhul/rovhul.c
@@ -0,0 +1,18 @@
+# include "rovhul.h"
+
+/* Navn: Ane Søgaard Jørgensen
+ * Dato: 18/10-2018
+ * Beskrivelse: Roevhul i C!
+ */
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+int main(void) {
+
+  printf("Yay!\n");
+
+  return 0;
+}
