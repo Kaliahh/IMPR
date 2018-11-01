ab95b63625e8a479b65fa341458a6e46984b3ec2
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2018-11-01 17:08:37 +0100

    "Færdig" med afl-8

--- impr/afleveringer/afl-8/afl-8.c
+++ impr/afleveringer/afl-8/afl-8.c
@@ -6,43 +6,84 @@
  */
 
 # include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
 # include <string.h>
-# include <math.h>
 # include <ctype.h>
 
+/* En maks længde på tekststrengen */
+# define MAX_LENGTH 20
+
 void identifier(char *, int, char*, char*, char*);
+void arrayFiller(char *, int);
 void displayInfo(char *, char *, char *);
 void scanCode(char *, int);
 
 int main(void) {
 
-  char code[50] = "ABC1234D56", warehouse[50], *id, *qualifers;
-  int n = strlen(code);
+  char code[MAX_LENGTH], warehouse[MAX_LENGTH], id[MAX_LENGTH], qualifers[MAX_LENGTH];
 
-  identifier(code, n, warehouse, id, qualifers);
+  /* Prompter brugeren for en tekststreng */
+  printf("Input code [CAPS][Max %d]: ", MAX_LENGTH);
+  fgets(code, MAX_LENGTH, stdin);
+  printf("\n");
+
+  /* Fylder de 3 arrays med \0 tegn */
+  arrayFiller(warehouse, MAX_LENGTH);
+  arrayFiller(id, MAX_LENGTH);
+  arrayFiller(qualifers, MAX_LENGTH);
+
+  /* Identificerer warehouse, id og qualifers */
+  identifier(code, strlen(code), warehouse, id, qualifers);
+
+  /* Printer warehouse, id og qualifers */
+  displayInfo(warehouse, id, qualifers);
 
   return 0;
 }
 
+/* Identificerer warehouse, id og qualifers */
+void identifier(char *code, int str_len, char* warehouse, char* id, char* qualifers) {
 
-void identifier(char *code, int n, char* warehouse, char* id, char* qualifers) {
+  int WAREHOUSE = 0, ID = 0;
+  int i = 0, o = 0;
 
-  int i = 0;
+  /* Checker for warehouse */
+  while (WAREHOUSE == 0) {
 
-  for (int i = 0; i < n; i++) {
-
-    if (isdigit(i) == 1) {
-
-      for (int j = 0; j <= i; j++) {
-        warehouse[j] = code[j];
-      }
+    /* Checker om den (int) typecastede værdi af code[i] er mellem 65 og 90 (Store bogstaver, ASCII) */
+    if ((int) code[i] >= 65 && (int) code[i] <= 90) {
+      warehouse[i] = code[i];
+      i++;
     }
 
+    /* WAREHOUSE flag sættes til 1, så det næste loop sættes i gang.
+     * o sættes til i, til brug af indeks senere */
+    else {
+      WAREHOUSE = 1;
+      o = i;
+    }
   }
 
-  displayInfo(warehouse, id, qualifers);
+  /* Checker for id */
+  while (WAREHOUSE == 1 && ID == 0) {
+
+    /* Checker om den (int) typecastede værdi af code[i] er mellem 48 og 57 (Tal, ASCII) */
+    if ((int) code[i] >= 48  && (int) code[i] <= 57) {
+      /* i - o, er for at starte ved id[0] */
+      id[i - o] = code[i];
+      i++;
+    }
+
+    else {
+      ID = 1;
+      o = i;
+    }
+  }
+
+  /* Checker for qualifers, op til længden af code tekststrengen */
+  while (WAREHOUSE == 1 && ID == 1 && i != str_len) {
+    qualifers[i - o] = code[i];
+    i++;
+  }
 }
 
 /* Printer resultatet til terminalen */
@@ -53,11 +94,9 @@ void displayInfo(char *warehouse, char *id, char *qualifers) {
   printf("Qualifers: %s\n", qualifers);
 }
 
-/*
-void scanCode(char *code, int n) {
-
+/* Fylder et char array med et givent tegn */
+void arrayFiller(char *array, int n) {
   for (int i = 0; i <= n; i++) {
-    scanf("%c", &code[i]);
+    array[i] = '\0';
   }
 }
-*/
